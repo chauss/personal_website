32324c085300e52dbb7770aba8c3049baa738967
Christopher Hauß <christopher@formigas.de>
2021-03-15 08:45:07 +0100

    added empty handler for locale-changed event

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
     import Tailwind from './utils/Tailwind.svelte';
     import LocaleSwitch from './components/LocaleSwitch.svelte';
+
+    const switchLocale = (event: CustomEvent) => {
+        console.log(`Switching locale to ${event.detail}`);
+    };
 </script>
 
 <Tailwind />
-<LocaleSwitch />
+<LocaleSwitch on:locale-changed={switchLocale} />
 <main class="container mx-auto">
     <h1 class="text-blue-500 p-6 hover:text-blue-300 text-center">Hello!</h1>
 </main>
