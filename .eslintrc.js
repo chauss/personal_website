2d85a84ec844cad4607d9786e2d4b47e10a73a51
Christopher Hauß <christopher@formigas.de>
2021-03-16 20:40:20 +0100

    added some more formatting rules and plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,18 @@
 module.exports = {
     parser: '@typescript-eslint/parser', // Specifies the ESLint parser
     extends: [
-        'plugin:@typescript-eslint/recommended', // Uses the recommended rules from @typescript-eslint/eslint-plugin
-        'plugin:prettier/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended', // Make sure this is always the last configuration in the extends array.
     ],
     plugins: [
-        'prettier', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+        'prettier', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors.
     ],
     parserOptions: {
         ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
         sourceType: 'module', // Allows for the use of imports
     },
     rules: {
-        'unused-imports/no-unused-imports': 2,
+        'unused-imports/no-unused-imports': 'error',
         // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
         // e.g. "@typescript-eslint/explicit-function-return-type": "off",
         'prettier/prettier': 'error',
