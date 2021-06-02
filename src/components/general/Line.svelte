81b553dd872a9ed6a5a193cfa877fbbdcbedb74b
Christopher Hauß <christopher@formigas.de>
2021-06-02 17:47:18 +0200

    not really working try to make lines between skillbubbles but does not work because of css transform

---
+++ src/components/general/Line.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    export let x1: number;
+    export let y1: number;
+    export let x2: number;
+    export let y2: number;
+
+    $: viewBoxWidth = Math.abs(Math.max(x1, x2) - Math.min(x1, x2));
+    $: viewBoxHeight = Math.abs(Math.max(y1, y2) - Math.min(y1, y2));
+</script>
+
+<svg width={viewBoxWidth} height={viewBoxHeight}>
+    <line class="line" {x1} {y1} {x2} {y2} />
+</svg>
+
+<style lang="scss">
+    .line {
+        stroke: var(--primaryColor);
+        stroke-width: 4px;
+    }
+</style>
--- src/components/skills/SkillSet.svelte
+++ src/components/skills/SkillSet.svelte
@@ -13,16 +13,25 @@
     export let degreeOffset = 0;
 
     const separation = 360 / skillSetObject.subSkills.length;
+
+    let mainSkill: HTMLElement;
+    let mainSkillPosition: [number, number];
+    $: if (mainSkill) {
+        mainSkillPosition = [
+            mainSkill.offsetLeft + mainSkill.clientWidth / 2,
+            mainSkill.offsetTop + mainSkill.clientHeight / 2,
+        ];
+    }
 </script>
 
-<div class="container">
+<div class="skill-set-container">
     <div class="main-skill-pos">
-        <SkillBubble name={skillSetObject.setName} size={SkillSize.large} />
+        <SkillBubble name={skillSetObject.setName} size={SkillSize.large} bind:element={mainSkill} />
     </div>
     {#each skillSetObject.subSkills as subSkill, idx}
         <div class="sub-skill-pos" use:cssVariables={{ rotation: `${separation * idx + degreeOffset}deg` }}>
             <div class="sub-skill-rot">
-                <SkillBubble name={subSkill} size={SkillSize.medium} />
+                <SkillBubble name={subSkill} size={SkillSize.medium} connectTo={mainSkillPosition} />
             </div>
         </div>
     {/each}
@@ -33,7 +42,8 @@
     $mediumBubbleSizeHalf: 44px;
     $totalSize: 2 * $radius + $mediumBubbleSizeHalf;
 
-    .container {
+    .skill-set-container {
+        position: relative;
         display: flex;
         width: $totalSize;
         height: $totalSize;
